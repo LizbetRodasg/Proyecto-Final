6335f22774413ab8ac7bc9c1ca7a537a60efa299
valeriaolivas <valeriaolivas54@gmail.com>
2024-06-08 23:10:01 -0700

    implementacion de diseño de cartas

--- Card.js
+++ Card.js
@@ -17,30 +17,29 @@ export default function Card({ onPress, isTurnedOver, children }) {
 }
 
 const styles = StyleSheet.create({
-    cardUp: {
-      width: 100,
-      height: 100,
-      margin: 10,
-      borderColor: "#334155",
-      borderRadius: 25, // Cambiar la cadena por un número
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#1e293b",
-    },
-    cardDown: {
-      width: 100,
-      height: 100,
-      margin: 10,
-      borderWidth: 10,
-      borderColor: "#334155",
-      borderRadius: 25, // Cambiar la cadena por un número
-      backgroundColor: "#1e293b",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    text: {
-      fontSize: 46,
-      color: "#334155",
-    },
-  });
-  
+  cardUp: {
+    width: 100,
+    height: 100,
+    margin: 10,
+    borderColor: "#334155",
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1e293b",
+  },
+  cardDown: {
+    width: 100,
+    height: 100,
+    margin: 10,
+    borderWidth: 10,
+    borderColor: "#334155",
+    borderRadius: 25,
+    backgroundColor: "#1e293b",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 46,
+    color: "#334155",
+  },
+});
